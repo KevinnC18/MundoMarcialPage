38784fca9c37924b028241408c7f61672375b411
KevinnC18 <kevincorreal1808@gmail.com>
2025-06-19 13:16:12 -0500

    Ajustes en el componente de carousel

--- mundomarcial-frontend/src/app/components/carousel/carousel.component.ts
+++ mundomarcial-frontend/src/app/components/carousel/carousel.component.ts
@@ -8,18 +8,5 @@ declare var bootstrap: any;
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent implements AfterViewInit {
-  ngAfterViewInit() {
-    const carouselElement = document.querySelector('#carouselMundoMarcial');
-    console.log('Carousel Element:', carouselElement); 
-    if (carouselElement) {
-      const carouselInstance = new bootstrap.Carousel(carouselElement, {
-        interval: 8000, 
-        ride: 'carousel',
-        touch: true
-      });
-    } else {
-      console.error('El elemento del carrusel no se encontró.');
-    }
-  }
+export class CarouselComponent {
 }
