b8893cef1f4bbb5bdcf9ecdd933c0749962e3cd0
KevinnC18 <kevincorreal1808@gmail.com>
2025-08-04 14:55:23 -0500

    Actualización de eventos y de galeria. Cambio en la organización de las capsulas

--- mundomarcial-frontend/src/app/app.component.ts
+++ mundomarcial-frontend/src/app/app.component.ts
@@ -9,8 +9,8 @@ import { trigger, transition, style, animate } from '@angular/animations';
   animations: [
     trigger('routeAnimations', [
       transition('* <=> *', [
-        style({ opacity: 0, transform: 'translateX(20px)' }),
-        animate('450ms ease', style({ opacity: 1, transform: 'translateY(0)' }))
+        style({ opacity: 0, transform: 'translateY(20px)' }),
+        animate('450ms ease', style({ opacity: 1, transform: 'translateX(0)' }))
       ])
     ])
   ]
